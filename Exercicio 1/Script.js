5c7b92355344c02ea4c8895aaaed46558551105a
Murilo <murilocm2005@gmail.com>
2026-04-29 21:05:14 -0500

    Add files via upload

---
+++ Exercicio 1/Script.js
@@ -0,0 +1,12 @@
+let contador = 10;
+
+const contagem = setInterval(() => {
+  if (contador > 0) {
+    console.log(contador);
+    contador--;
+  } else {
+    console.log("0");
+    console.log("DECOLAGEM! 🚀");
+    clearInterval(contagem); // Para o cronômetro
+  }
+}, 1000);
---
+++ Exercicio 2/Script.js
@@ -0,0 +1,8 @@
+let n = parseInt(prompt("Digite um numero inteiro positivo:"));
+
+console.log("Soma dos números de " + n);
+
+for (let i = 1; i <= n; i++);{
+    let resultado = n + 1;
+    console.log(n + " + " + i + " = " + resultado); 
+}
